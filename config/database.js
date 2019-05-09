50fcf064188e840f60c5845f3138d0e7c808e0dd
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-05-09 12:08:11 -0400

    heroku deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    url: 'mongodb+srv://ironhack:ironhack@cluster0-uqv35.mongodb.net/test?retryWrites=true' //'mongodb://localhost/secondProject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    url: 'mongodb+srv://ironhack:ironhack@cluster0-uqv35.mongodb.net/test'
+    //'mongodb://localhost/secondProject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
